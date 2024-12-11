bea4af34166b040eed1f1541590fb767cad80b96
kushalk47 <115680607+kushalk47@users.noreply.github.com>
2024-12-11 22:36:33 +0530

    Update Home.py

--- .venv/Home.py
+++ .venv/Home.py
@@ -7,7 +7,7 @@ st.set_page_config(page_title='Kushal Portfolio',layout='wide')
 col1,empt_col,col2 = st.columns([1.5,0.2,2.0])
 
 with col1:
-    st.image(r"https://github.com/kushalk47/Resume/blob/master/photos/image2.jpg ", width=600)
+    st.image(r"https://raw.githubusercontent.com/kushalk47/Resume/master/photos/image2.jpg", width=600)
 
 with col2:
     st.title("Kushal Kadur")
@@ -51,7 +51,7 @@ col7,emp_col,col8 = st.columns([1.5,0.5,1.5])
 
 with col7:
     st.header("Weather Forecast App")
-    st.image(r"https://github.com/kushalk47/Resume/blob/master/photos/xy.png", width=300)
+    st.image(r"https://raw.githubusercontent.com/kushalk47/Resume/master/photos/xy.png", width=300)
     desc = '''This is a Weather Forecast app that predicts the weather for up to 5 days.'''
     st.write(desc)
     st.write("[Source Code](https://github.com/kushalk47)")
@@ -59,26 +59,26 @@ with col7:
     #st.header("")
 
     st.header("To-Do App")
-    st.image(r"https://github.com/kushalk47/Resume/blob/master/photos/1.png  ", width=300)
+    st.image(r"https://raw.githubusercontent.com/kushalk47/Resume/master/photos/1.png", width=300)
     desc = '''A simple To-Do application to help avoid distractions.'''
     st.write(desc)
     st.write("[Source Code](https://todoapp47.streamlit.app)")
 
     st.header("Chatbot (ChatGPT API Key)")
-    st.image(r"https://github.com/kushalk47/Resume/blob/master/photos/9.png", width=300)
+    st.image(r"https://raw.githubusercontent.com/kushalk47/Resume/master/photos/9.png", width=300)
     desc = '''An intelligent chatbot created using the ChatGPT API key.'''
     st.write(desc)
     st.write("[Source Code](https://github.com/kushalk47)")
 
 with col8:
     st.header("Market Basket Analysis")
-    st.image(r"https://github.com/kushalk47/Resume/blob/master/photos/market_basket_analysis.jpg", width=280)
+    st.image(r"https://raw.githubusercontent.com/kushalk47/Resume/master/photos/market_basket_analysis.jpg", width=280)
     desc = '''Using the Apriori algorithm and K-Means clustering to analyze frequently bought items and cluster customers into different groups.'''
     st.write(desc)
     st.write("[Source Code](https://github.com/kushalk47)")
 
     st.header("Data Analytics (Insurance & UPI)")
-    st.image(r"https://github.com/kushalk47/Resume/blob/master/photos/15.png", width=300)
+    st.image(r"https://raw.githubusercontent.com/kushalk47/Resume/master/photos/market_basket_analysis.jpg", width=300)
     desc = '''Data analytics project using libraries like NumPy, pandas, seaborn, and SQL to analyze insurance and UPI sales data.'''
     st.write(desc)
     st.write("[Source Code](https://github.com/kushalk47)")
